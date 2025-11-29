d19d54317478b9cda257c4409e335975220a7797
nicolas almeida <contato@ravt.com.br>
2025-11-29 13:13:29 -0300

    feat: create enviroment util

---
+++ src/utils/enviroments.utils.ts
@@ -0,0 +1,70 @@
+import 'dotenv/config';
+class TEnviroments {
+  DATABASE_HOST: string;
+  DATABASE_PORT: number;
+  DATABASE_USER: string;
+  DATABASE_PASSWORD: string;
+  DATABASE_DATABASE: string;
+  PORT: number;
+  MODE: 'DEV' | 'PRD' | 'TST';
+  DATABASE_TYPE: 'postgres' | 'mysql';
+}
+
+enum EEnviroments {
+  DATABASE_HOST = 'DATABASE_HOST',
+  DATABASE_PORT = 'DATABASE_PORT',
+  DATABASE_USER = 'DATABASE_USER',
+  DATABASE_PASSWORD = 'DATABASE_PASSWORD',
+  DATABASE_DATABASE = 'DATABASE_DATABASE',
+  PORT = 'PORT',
+  MODE = 'MODE',
+  DATABASE_TYPE = 'DATABASE_TYPE',
+}
+
+const toNumber: Array<string> = [EEnviroments.DATABASE_PORT, EEnviroments.PORT];
+
+class Enviroments {
+  constructor(private enviroment: NodeJS.ProcessEnv) {}
+
+  public envs: TEnviroments = {
+    DATABASE_HOST: '',
+    DATABASE_PORT: 5432,
+    DATABASE_USER: '',
+    DATABASE_PASSWORD: '',
+    DATABASE_DATABASE: '',
+    PORT: 3001,
+    MODE: 'DEV',
+    DATABASE_TYPE: 'postgres',
+  };
+
+  private checkValueType(env: string, eEnv: string): number | string {
+    if (toNumber.find((env) => env === eEnv)) return parseFloat(env);
+    return env;
+  }
+
+  private validateValue(eEnv: string): void {
+    const value = this.enviroment[eEnv];
+    if (!value) {
+      console.log('Missing enviroment variable:', eEnv);
+      throw new Error(`config error - missing env.${eEnv}`);
+    }
+    this.envs[eEnv] = this.checkValueType(value, eEnv);
+  }
+
+  public setValues(enviroments: Array<EEnviroments>) {
+    enviroments.forEach((env) => this.validateValue(env));
+    return this;
+  }
+
+  public isProduction(): boolean {
+    return this.envs.MODE === 'PRD';
+  }
+}
+
+const configEnviroments = new Enviroments(process.env).setValues(
+  Object.values(EEnviroments),
+);
+
+const enviroments: TEnviroments = configEnviroments.envs;
+
+export { Enviroments, configEnviroments, enviroments };
